a1fbc1333f542dd84149c11ae0f317d6ca23814f
Bruno Scherer <bruno.raphael96@gmail.com>
2021-11-08 22:30:29 -0300

    chore: remove unused enzyme dependencies

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,1 @@
-/* eslint-disable @typescript-eslint/no-var-requires,@typescript-eslint/naming-convention */
-
 import '@testing-library/jest-dom';
-const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
-const Enzyme = require('enzyme');
-
-Enzyme.configure({ adapter: new Adapter() });
